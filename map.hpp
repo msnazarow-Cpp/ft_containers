9d8f086f4203d4d8eed21b61ae9d7cf960af3514
msnazarow <msnazarow@gmail.com>
2021-07-16 17:04:32 +0300

    previous

--- map.hpp
+++ map.hpp
@@ -1,5 +1,8 @@
 #include "utils.hpp"
-
+#include "reverse_iterator.hpp"
+#include <cstdlib>
+#include <iterator>
+namespace ft{
 	template < class Key,                                     // map::key_type
 			class T,                                       // map::mapped_type
 			class Compare = ft::less<Key>,                     // map::key_compare
@@ -8,19 +11,34 @@
 			{
 				typedef Key key_type;
 				typedef T mapped_type;
-				typedef ft::pair(const key_type, mapped_type) value_type;
-				typedef Comapare key_compare;
+				typedef ft::pair<const key_type, mapped_type> value_type;
+				typedef Compare key_compare;
+				class value_compare
+				{
+					friend class map;
+					protected:
+					Compare comp;
+					value_compare (Compare c) : comp(c) {}  // constructed with map's comparison object
+					public:
+					typedef bool result_type;
+					typedef value_type first_argument_type;
+					typedef value_type second_argument_type;
+					bool operator() (const value_type& x, const value_type& y) const
+					{
+						return comp(x.first, y.first);
+					}
+				};
 				typedef Alloc allocator_type;
-				typedef allocator_type::reference reference;
-				typedef allocator_type::const_reference const_reference;
-				typedef allocator_type::pointer pointer;
-				typedef allocator_type::const_pointer const_pointer;
-				typedef allocator_type::difference_type difference_type;
-				typedef allocator_type::size_type size_type;
+				typedef typename allocator_type::reference reference;
+				typedef typename allocator_type::const_reference const_reference;
+				typedef typename allocator_type::pointer pointer;
+				typedef typename allocator_type::const_pointer const_pointer;
+				typedef typename allocator_type::difference_type difference_type;
+				typedef typename allocator_type::size_type size_type;
 				typedef map_iterator<value_type> iterator;
 				typedef map_iterator<const value_type> const_iterator;
-				typedef reverse_iterator<iterator> reverse_iterator;
-				typedef reverse_iterator<const_iterator> const_reverse_iterator;
+				typedef ft::reverse_iterator<iterator> reverse_iterator;
+				typedef ft::reverse_iterator<const_iterator> const_reverse_iterator;
 
 				explicit map (const key_compare& comp = key_compare(),
 				const allocator_type& alloc = allocator_type());
@@ -62,4 +80,6 @@
 				const_iterator upper_bound (const key_type& k) const;
 				pair<const_iterator,const_iterator> equal_range (const key_type& k) const; 
 				pair<iterator,iterator> equal_range (const key_type& k);
-			}
+			};
+
+}
--- utils.hpp
+++ utils.hpp
@@ -71,11 +71,51 @@ template <class T>
 		}
 		return (first1 == last1) && (first2 != last2);
 	}
-		template <typename U>
+	template <typename U>
 	void swap(U& a, U&b)
 	{
 		U tmp = a;
 		a = b;
 		b = tmp;
+	}
+	template <class T1, class T2> struct pair
+	{
+		typedef T1 first_type;
+		typedef T2 second_type;
+		first_type first;
+		second_type second;
+		pair();
+		template<class U, class V> pair (const pair<U,V>& pr);
+		pair (const first_type& a, const second_type& b);
+		pair& operator= (const pair& pr);
+		
+	};
+	template <class T1, class T2>
+	bool operator== (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return lhs.first==rhs.first && lhs.second==rhs.second; }
+
+	template <class T1, class T2>
+	bool operator!= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return !(lhs==rhs); }
+
+	template <class T1, class T2>
+	bool operator<  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return lhs.first<rhs.first || (!(rhs.first<lhs.first) && lhs.second<rhs.second); }
+
+	template <class T1, class T2>
+	bool operator<= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return !(rhs<lhs); }
+
+	template <class T1, class T2>
+	bool operator>  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return rhs<lhs; }
+
+	template <class T1, class T2>
+	bool operator>= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return !(lhs<rhs); }
+	template <class T1,class T2>
+	pair<T1,T2> make_pair (T1 x, T2 y)
+	{
+		return ( pair<T1,T2>(x,y) );
 	}
 }
